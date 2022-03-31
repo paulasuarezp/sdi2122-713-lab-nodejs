dd2a81e2fd2b5330b333cb0c7df69773f1db8900
uo269745 <71699020+uo269745@users.noreply.github.com>
2022-03-31 16:20:53 +0200

    SDI-2122-713-8.3-Subida a ficheros y gestión de colecciones.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -6,6 +6,13 @@ var logger = require('morgan');
 
 
 var app = express();
+let fileUpload = require('express-fileupload');
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024 * 1024 },
+  createParentPath: true
+}));
+app.set('uploadPath', __dirname)
+
 
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
