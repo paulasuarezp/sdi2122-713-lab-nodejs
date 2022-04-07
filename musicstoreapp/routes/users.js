4f80a69dd83d6373b5c8f3cb899d40ed73e86ff0
uo269745 <71699020+uo269745@users.noreply.github.com>
2022-04-07 10:35:22 +0200

    SDI-2122-713-9.3-Mensajes y alertas.

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -14,9 +14,10 @@ module.exports = function (app, usersRepository) {
       password: securePassword
     }
     usersRepository.insertUser(user).then(userId => {
-      res.redirect("/users/login");
+      res.redirect("/users/login" + "?message=Nuevo usuario registrado." + "&messageType=alert-info");
     }).catch(error => {
-      res.send("Error al insertar el usuario");
+      res.redirect("/users/signup" + "?message=Se ha producido un error al registrar al nuevo usuario."
+          + "&messageType=alert-danger");
     });
   })
 
@@ -24,32 +25,38 @@ module.exports = function (app, usersRepository) {
     res.render("login.twig");
   })
 
-  app.post('/users/login', function(req,res){
+  app.post('/users/login', function (req, res) {
     let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
         .update(req.body.password).digest('hex');
-
     let filter = {
       email: req.body.email,
       password: securePassword
     }
-
     let options = {};
-
-    usersRepository.findUser(filter,options).then(user =>{
-      if(user == null){
+    usersRepository.findUser(filter, options).then(user => {
+      if (user == null) {
         req.session.user = null;
-        res.send("Usuario no identificado");
+//res.send("Usuario no identificado");
+        res.redirect("/users/login" +
+            "?message=Email o password incorrecto"+
+            "&messageType=alert-danger ");
       } else {
         req.session.user = user.email;
+        //res.send("Usuario identificado correctamente: " + user.email
         res.redirect("/publications");
       }
     }).catch(error => {
       req.session.user = null;
-      res.send("Se ha producido un error al buscar el usuario")
+      //res.send("Se ha producido un error al buscar el usuario: " + error)
+      res.redirect("/users/login" +
+          "?message=Se ha producido un error al buscar el usuario"+
+          "&messageType=alert-danger ");
     })
   })
 
-  app.get('/users/logout', function (req, res) {
+
+
+        app.get('/users/logout', function (req, res) {
     req.session.user = null;
     res.send("El usuario se ha desconectado correctamente");
   })
